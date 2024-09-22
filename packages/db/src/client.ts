4ba350e0da337c3f022a4927610960b5e900668c
Jay McMullen <j@1f.io>
2024-09-22 18:57:06 +1000

    feat: connect db

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,6 +1,14 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { SQLDatabase } from "encore.dev/storage/sqldb";
+import postgres from "postgres";
 
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
+export const sql = new SQLDatabase("url", {
+  migrations: "./migrations",
+});
+
+const connectionString = sql.connectionString;
+const client = postgres(connectionString);
+
+export const db = drizzle(client, { schema });
