fc617bea896fc8940c632824d42f3acf6961856d
Jay McMullen <j@1f.io>
2024-09-23 11:55:36 +1000

    fix: base urls

---
+++ apps/nextjs/src/app/lib/base-url.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-restricted-properties */
+export const getBaseUrl = () => {
+  if (typeof window !== "undefined") return window.location.origin;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+};
